b8899c0df7869ef041eae2d1334ed1eb5a687751
suyeon <qorhsldk159@hanmail.net>
2021-07-14 19:49:21 +0900

    로그인세션

--- src/controller/CmtController.java
+++ src/controller/CmtController.java
@@ -49,9 +49,8 @@ public class CmtController extends HttpServlet {
 					  String comment = request.getParameter("comment");
 			    	  int game_seq = Integer.parseInt(request.getParameter("game_seq"));		    	 
 
-			    	  //MemberDTO dto = (MemberDTO)request.getSession().getAttribute("login");
-			    	  //String id =dto.getId();
-			    	  String id ="su1234";
+			    	  MemberDTO dto = (MemberDTO)request.getSession().getAttribute("login");
+			    	  String id =dto.getId();
 
 			    	  int result = cdao.insert(id,comment,game_seq);
 
